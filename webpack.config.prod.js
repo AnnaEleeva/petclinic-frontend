05975d207453e8853e69452020af22515b7a3270
AnnaE <anna_eleeva@yahoo.com>
2024-05-04 01:52:09 +0500

    publicPath: '/dist/',

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public/dist'),
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: '/dist/',
     /* redbox-react/README.md */
     // ,devtoolModuleFilenameTemplate: '/[absolute-resource-path]'
   },
